60e90e57ef86a1df0d01bbfe210604de5bb23f18
Keval <keval@middleware.io>
2023-08-22 14:14:30 +0530

    testing with cjs format

--- configs/tsup.base.config.js
+++ configs/tsup.base.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    format: ["esm"],
+    format: ["cjs"],
     splitting: true,
     clean: true,
     dts: true,
